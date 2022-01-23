93a31eab59e796f82ff5b92caf3af2db65c82206
smirnoffalexx <smirnov.am16@physics.msu.ru>
2022-01-23 23:56:50 +0300

    debug catcher

--- bot.py
+++ bot.py
@@ -111,7 +111,7 @@ def catch_phrase(message):
 	if switcher:
 		for key in words.keys():
 			if message.text.find(key) == -1:
-				pass
+				bot.reply_to(message, "Тут нет ключевых слов")
 			else:
 				bot.reply_to(message, words[key])
 
