a93074ebd9c616c308938096462fed8abcd5aa1f
smirnoffalexx <smirnov.am16@physics.msu.ru>
2022-05-24 23:54:01 +0300

    add congratulation to another thread

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import datetime
 import julian
 import config
 # from re import search
-
-bot = telebot.TeleBot(config.token)
+import threading # import Thread
+import time
 
 switchers = {}
 
-group_chat_id = "818106094" # -1001581543375
+group_chat_id = "-1001581543375" # 818106094
 
 birthdays = {
 	"Сережа": datetime.datetime(1999, 4, 9, hour=9), # "09.04",
@@ -48,19 +48,34 @@ def store_switcher(my_key, my_value):
 def get_switcher(my_key):
  	return switchers[my_key].get('value')
 
-def congratulation():
-	current_time = datetime.datetime.now()
-	if (current_time.hour == 19) and (current_time.minute == 9):
-		date = current_time.date()
-		for key in birthdays.keys():
-			if (date.day == birthdays[key].date().day) and (date.month == birthdays[key].date().month):
-				bot.send_message(group_chat_id, "{0}, Поздравляю с Днем Рождения!!!".format(key))
-				picture = open("Gosling_for_DR.jpg","rb")
-				bot.send_photo(group_chat_id, picture)
-				f = open("for_dr.mp4","rb")
-				bot.send_document(group_chat_id, f)
-				time.sleep(60)
+def congratulation(bot):
+	i = 0;
+	while True:
+		try:
+			i = i + 1
+			print(i)
+			current_time = datetime.datetime.now()
+			if (current_time.hour == 10) and (current_time.minute == 0):
+				date = current_time.date()
+				for key in birthdays.keys():
+					if (date.day == birthdays[key].date().day) and (date.month == birthdays[key].date().month):
+						bot.send_message(group_chat_id, "{0}, Поздравляю с Днем Рождения!!!".format(key))
+						picture = open("Gosling_for_DR.jpg","rb")
+						bot.send_photo(group_chat_id, picture)
+						f = open("for_dr.mp4","rb")
+						bot.send_document(group_chat_id, f)
+						# time.sleep(60)
+		except Exception:
+			pass
+		except KeyboardInterrupt:
+			print("KeyboardInterrupt raised")
+			break
+		time.sleep(60)
 
+bot = telebot.TeleBot(config.token)
+
+threading.Thread(target=congratulation, args=(bot,)).start()
+    
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет, Суетолог! Я - Бот Макара.\n Для того, чтобы узнать работает ли сегодня Макар - напиши /today.\n\
